30939fc7b32827d6dabc6fecc04c95643585a756
kostya05983 <kostya05983@mail.ru>
2019-03-01 23:42:52 +0700

    init tutorials

---
+++ src/03_state/Lesson.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+//Modern
+class CounterButton extends Component {
+    state = {
+        counter: 0,
+    }
+
+    handleClick = () => {
+        this.setState({
+            counter: 2,
+        })
+    }
+
+    render() {
+        const { counter } = this.state;
+        //
+        return (
+            <div>
+            <div>{counter}</div>
+                <button onClick={this.handleClick}>+1</button>
+            </div>
+        );
+    }
+}
+
+export default CounterButton;
