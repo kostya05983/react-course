b0402def493d6698e4db7ec31afac3a01659a312
kostya05983 <kostya05983@mail.ru>
2019-03-02 20:40:18 +0700

    Finish with the 13th Lesson

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import Form from "./12_refs/Lesson";
+import Form from "./13_high/Lesson";
 
 ReactDOM.render(<Form/>, document.getElementById('root'));
 registerServiceWorker();
