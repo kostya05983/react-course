2a6bf8a6c1f41d13d53b382d284c020456da646b
kostya05983 <kostya05983@mail.ru>
2019-03-06 19:46:49 +0700

    [ADD] finish with 15 lesson context

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import  Lesson from "./14_portals/Lesson";
+import  Lesson from "./15_context/Lesson";
 
 ReactDOM.render(<Lesson/>, document.getElementById('root'));
 registerServiceWorker();
